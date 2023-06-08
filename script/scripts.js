bd4b0fd5ffa0edf80fe199820488c15aeb308879
rafael <rafaelcnunes@icloud.com>
2023-06-08 20:36:26 -0300

    added graphic

--- script/scripts.js
+++ script/scripts.js
@@ -3,11 +3,11 @@ const graficoDolar = document.getElementById("graficoDolar");
 const graficoParaDolar = new Chart(graficoDolar, {
   type: "line",
   data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    labels: [],
     datasets: [
       {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
+        label: "Dólar",
+        data: [],
         borderWidth: 1,
       },
     ],
@@ -19,15 +19,27 @@ async function conectaAPI() {
     "https://economia.awesomeapi.com.br/json/last/USD-BRL"
   );
   const conectaTraduzido = await conecta.json();
-  console.log(conectaTraduzido);
+  const valorDolar = conectaTraduzido.USDBRL.high;
+
+  adicionarDados(graficoParaDolar, geraHorario(), valorDolar);
 }
 
 function geraHorario() {
-    let data = new Date();
+  let data = new Date();
 
-    let hora = data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
+  let hora =
+    data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
 
-    return hora;
+  return hora;
+}
+
+function adicionarDados(grafico, legenda, dados) {
+  grafico.data.labels.push(legenda);
+  grafico.data.datasets.forEach((dataset) => {
+    dataset.data.push(dados);
+  });
+
+  grafico.update();
 }
 
 setInterval(conectaAPI, 5000);
