5033e0a262e58603000c8ec6633387e58d8cbb59
rafael <rafaelcnunes@icloud.com>
2023-06-08 19:48:06 -0300

    added getHorario

--- script/scripts.js
+++ script/scripts.js
@@ -1,21 +1,34 @@
-const graficoDolar = document.getElementById('graficoDolar');
+const graficoDolar = document.getElementById("graficoDolar");
 
 const graficoParaDolar = new Chart(graficoDolar, {
-    type: 'line',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1
-        }]
-    },
+  type: "line",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
 });
 
 async function conectaAPI() {
-    const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL");
-    const conectaTraduzido = await conecta.json();
-    console.log(conectaTraduzido);
+  const conecta = await fetch(
+    "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+  );
+  const conectaTraduzido = await conecta.json();
+  console.log(conectaTraduzido);
 }
 
-conectaAPI();
+function geraHorario() {
+    let data = new Date();
+
+    let hora = data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
+
+    return hora;
+}
+
+setInterval(conectaAPI, 5000);
+setInterval(geraHorario, 1000);
